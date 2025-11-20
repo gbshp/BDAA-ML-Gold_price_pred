c97ac130635433eb09b8fe07b42fbf40c0d42c42
gbshp <pranaykr636@gmail.com>
2025-11-20 18:11:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-filepath = '.venv\archive.zip_unzipped\All_Historical_Data_Separately\Geopolitical Risk Index Daily.csv'
+filepath = '.venv\\archive.zip_unzipped\\Gold-Silver-GeopoliticalRisk_HistoricalData.csv'
 df = pd.read_csv(filepath)
+print(df.head())
 
-print(df.head())    
-print(df.shape)
+
+# df = df.drop_duplicates()
+# df = df.dropna()
+# df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
+
+cat_col = [col for col in df.columns if df[col].dtype == 'object']
+num_col = [col for col in df.columns if df[col].dtype != 'object']
+
+print('Categorical columns:', cat_col)
+print('Numerical columns:', num_col)    
